61ba939a914f0d149928b21846cbaa4cfe6b4ef3
hardikothiya <hardikothiya@gmail.com>
2022-01-13 08:48:31 +0530

    google sign in completed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'forgot_password.dart';
 import 'home_page.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   static const id = 'LoginPage';
@@ -18,6 +19,8 @@ class _LoginScreenState extends State<LoginScreen> with InputValidationMixin {
 
   final storage = FlutterSecureStorage();
   FirebaseAuth auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
   final textEditingController = TextEditingController();
   final passwordEditingController = TextEditingController();
   late String _password;
@@ -134,7 +137,7 @@ class _LoginScreenState extends State<LoginScreen> with InputValidationMixin {
                           value: credentials.user?.email.toString());
                       print(credentials.user?.email);
 
-                      Navigator.pushNamed(context, HomeScreen.id);
+                      Navigator.pushReplacementNamed(context, HomeScreen.id);
                       textEditingController.clear();
 
                       passwordEditingController.clear();
@@ -160,6 +163,42 @@ class _LoginScreenState extends State<LoginScreen> with InputValidationMixin {
                   'Forgot Password ?',
                   style: TextStyle(
                       color: Colors.blue, letterSpacing: 0.8, fontSize: 18),
+                ),
+              ),
+              InkWell(
+                onTap: () async {
+                  try {
+                    GoogleSignInAccount? usercredentials =
+                        await googleSignIn.signIn();
+
+                    Navigator.pushNamed(context, HomeScreen.id);
+
+                    await storage.write(
+                        key: 'userID', value: usercredentials?.id);
+                    await storage.write(
+                        key: 'email', value: usercredentials?.email);
+
+                    print(usercredentials);
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'account-exists-with-different-credential') {
+                      Fluttertoast.showToast(msg: 'User exist');
+                    } else if (e.code == 'invalid-credential') {
+                      Fluttertoast.showToast(msg: 'Invalid credentials');
+                    }
+                  }
+                },
+                child: Ink(
+                  color: Color(0xFF397AF3),
+                  child: Padding(
+                    padding: EdgeInsets.all(6),
+                    child: Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: const [
+                        SizedBox(width: 12),
+                        Text('Sign in with Google'),
+                      ],
+                    ),
+                  ),
                 ),
               )
             ],
